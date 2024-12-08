05cb200673296923c9de71e3daea8ebd8bf0e883
unknown <dixondaniel434@gmail.com>
2024-12-08 21:46:50 +0200

    Imports and training data for retrival model

---
+++ backend/model/retrieval_model.py
@@ -0,0 +1,21 @@
+import os
+import pprint
+import tempfile
+
+from typing import Dict, Text
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_recommenders as tfrs
+
+# Ratings data.
+ratings = tfds.load("movielens/100k-ratings", split="train")
+# Features of all the available movies.
+movies = tfds.load("movielens/100k-movies", split="train")
+
+for x in ratings.take(1).as_numpy_iterator():
+  pprint.pprint(x)
+
+for x in movies.take(1).as_numpy_iterator():
+  pprint.pprint(x)
