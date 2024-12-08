4fd0973fea4d77622ccc81ac5728b0220069faef
unknown <dixondaniel434@gmail.com>
2024-12-08 22:59:37 +0200

    Model to minimize loss and model to loop training

--- backend/model/retrieval_model.py
+++ backend/model/retrieval_model.py
@@ -64,3 +64,167 @@ task = tfrs.tasks.Retrieval(
   metrics=metrics
 )
 
+class MovielensModel(tfrs.Model):
+
+  def __init__(self, user_model, movie_model):
+    super().__init__()
+    self.movie_model: tf.keras.Model = movie_model
+    self.user_model: tf.keras.Model = user_model
+    self.task: tf.keras.layers.Layer = task
+
+  def compute_loss(self, features: Dict[Text, tf.Tensor], training=False) -> tf.Tensor:
+    # We pick out the user features and pass them into the user model.
+    user_embeddings = self.user_model(features["user_id"])
+    # And pick out the movie features and pass them into the movie model,
+    # getting embeddings back.
+    positive_movie_embeddings = self.movie_model(features["movie_title"])
+
+    # The task computes the loss and the metrics.
+    return self.task(user_embeddings, positive_movie_embeddings)
+
+
+
+import os
+import pprint
+import tempfile
+
+from typing import Dict, Text
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_recommenders as tfrs
+
+# Ratings data.
+ratings = tfds.load("movielens/100k-ratings", split="train")
+# Features of all the available movies.
+movies = tfds.load("movielens/100k-movies", split="train")
+
+# map the ratings and movies data and extract movie_title and user_id
+ratings = ratings.map(lambda x: {
+    "movie_title": x["movie_title"],
+    "user_id": x["user_id"],
+})
+movies = movies.map(lambda x: x["movie_title"])
+
+#shuffle the data
+tf.random.set_seed(42)
+shuffled = ratings.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
+
+#divide the data into train and test data
+train = shuffled.take(80_000)
+test = shuffled.skip(80_000).take(20_000)
+
+#get movie titles and user_ids
+movie_titles = movies.batch(1_000)
+user_ids = ratings.batch(1_000_000).map(lambda x: x["user_id"])
+
+#get different movies tiles and user ids
+unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
+unique_user_ids = np.unique(np.concatenate(list(user_ids)))
+
+unique_movie_titles[:10]
+
+#the query tower
+embedding_dimension = 32
+
+user_model = tf.keras.Sequential([
+  tf.keras.layers.StringLookup(
+      vocabulary=unique_user_ids, mask_token=None),
+  # We add an additional embedding to account for unknown tokens.
+  tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+])
+
+#the candidate tower
+movie_model = tf.keras.Sequential([
+  tf.keras.layers.StringLookup(
+      vocabulary=unique_movie_titles, mask_token=None),
+  tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding_dimension)
+])
+
+metrics = tfrs.metrics.FactorizedTopK(
+  candidates=movies.batch(128).map(movie_model)
+)
+
+task = tfrs.tasks.Retrieval(
+  metrics=metrics
+)
+
+#joins user and movie embeddings with a retrieval task to minimize the loss for better recommendations
+class MovielensModel(tfrs.Model):
+
+  def __init__(self, user_model, movie_model):
+    super().__init__()
+    self.movie_model: tf.keras.Model = movie_model
+    self.user_model: tf.keras.Model = user_model
+    self.task: tf.keras.layers.Layer = task
+
+  def compute_loss(self, features: Dict[Text, tf.Tensor], training=False) -> tf.Tensor:
+    # We pick out the user features and pass them into the user model.
+    user_embeddings = self.user_model(features["user_id"])
+    # And pick out the movie features and pass them into the movie model,
+    # getting embeddings back.
+    positive_movie_embeddings = self.movie_model(features["movie_title"])
+
+    # The task computes the loss and the metrics.
+    return self.task(user_embeddings, positive_movie_embeddings)
+  
+# Handles how training is executed in a custom loop 
+class NoBaseClassMovielensModel(tf.keras.Model):
+
+    def __init__(self, user_model, movie_model):
+        super().__init__()
+        self.movie_model: tf.keras.Model = movie_model
+        self.user_model: tf.keras.Model = user_model
+        self.task: tf.keras.layers.Layer = task
+
+    def train_step(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+
+        # Set up a gradient tape to record gradients.
+        with tf.GradientTape() as tape:
+
+            # Loss computation.
+            user_embeddings = self.user_model(features["user_id"])
+            positive_movie_embeddings = self.movie_model(features["movie_title"])
+            loss = self.task(user_embeddings, positive_movie_embeddings)
+
+            # Handle regularization losses as well.
+            regularization_loss = sum(self.losses)
+
+            total_loss = loss + regularization_loss
+
+        gradients = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        metrics = {metric.name: metric.result() for metric in self.metrics}
+        metrics["loss"] = loss
+        metrics["regularization_loss"] = regularization_loss
+        metrics["total_loss"] = total_loss
+
+        return metrics
+
+    def test_step(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+
+        # Loss computation.
+        user_embeddings = self.user_model(features["user_id"])
+        positive_movie_embeddings = self.movie_model(features["movie_title"])
+        loss = self.task(user_embeddings, positive_movie_embeddings)
+
+        # Handle regularization losses as well.
+        regularization_loss = sum(self.losses)
+
+        total_loss = loss + regularization_loss
+
+        metrics = {metric.name: metric.result() for metric in self.metrics}
+        metrics["loss"] = loss
+        metrics["regularization_loss"] = regularization_loss
+        metrics["total_loss"] = total_loss
+
+        return metrics
+
+    
+
+
+  
+    
+
