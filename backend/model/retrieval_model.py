fc4cbc4edf81469286aa0baeb67dc13ef7c3c3d7
unknown <dixondaniel434@gmail.com>
2024-12-08 22:29:10 +0200

    Creatung query and candidate tower for the retrieval model

--- backend/model/retrieval_model.py
+++ backend/model/retrieval_model.py
@@ -14,8 +14,45 @@ ratings = tfds.load("movielens/100k-ratings", split="train")
 # Features of all the available movies.
 movies = tfds.load("movielens/100k-movies", split="train")
 
-for x in ratings.take(1).as_numpy_iterator():
-  pprint.pprint(x)
+# map the ratings and movies data and extract movie_title and user_id
+ratings = ratings.map(lambda x: {
+    "movie_title": x["movie_title"],
+    "user_id": x["user_id"],
+})
+movies = movies.map(lambda x: x["movie_title"])
 
-for x in movies.take(1).as_numpy_iterator():
-  pprint.pprint(x)
+#shuffle the data
+tf.random.set_seed(42)
+shuffled = ratings.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
+
+#divide the data into train and test data
+train = shuffled.take(80_000)
+test = shuffled.skip(80_000).take(20_000)
+
+#get movie titles and user_ids
+movie_titles = movies.batch(1_000)
+user_ids = ratings.batch(1_000_000).map(lambda x: x["user_id"])
+
+#get different movies tiles and user ids
+unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
+unique_user_ids = np.unique(np.concatenate(list(user_ids)))
+
+unique_movie_titles[:10]
+
+#the query tower
+embedding_dimension = 32
+
+user_model = tf.keras.Sequential([
+  tf.keras.layers.StringLookup(
+      vocabulary=unique_user_ids, mask_token=None),
+  # We add an additional embedding to account for unknown tokens.
+  tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+])
+
+#the candidate tower
+movie_model = tf.keras.Sequential([
+  tf.keras.layers.StringLookup(
+      vocabulary=unique_movie_titles, mask_token=None),
+  tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding_dimension)
+])
+
