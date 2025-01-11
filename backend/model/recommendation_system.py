1cd4a6019b0c893a0df4191e2406f24b5871b1c3
unknown <dixondaniel434@gmail.com>
2025-01-12 01:00:43 +0200

    Clean the column names

--- backend/model/recommendation_system.py
+++ backend/model/recommendation_system.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.neighbors import NearestNeighbors
 
 
-movies = pd.read_csv('../data/movies.csv')
+movies = pd.read_csv('../data/TMDB_movie_dataset_v11.csv')
 ratings = pd.read_csv('../data/ratings.csv')
 
 # Removing duplicate rows
@@ -14,14 +14,26 @@ ratings.drop_duplicates(inplace=True)
 movies.dropna(inplace=True)
 ratings.dropna(inplace=True)
 
+# Display first few rows of the dataset
+movies.head()
+
+# Clean up the column names
+movies.columns = movies.columns.str.replace('"', '').str.strip()
+
+# Spliting up the data to rest and train
+
+
 # Extracting the genres column
 genres = movies['genres']
+
 
 # Creating an instance of the OneHotEncoder
 encoder = OneHotEncoder()
 
 # Fitting and transforming the genres column
 genres_encoded = encoder.fit_transform(genres.values.reshape(-1, 1))
+
+# print(genres_encoded)
 
 # Creating an instance of the NearestNeighbors class
 recommender = NearestNeighbors(metric='cosine')
