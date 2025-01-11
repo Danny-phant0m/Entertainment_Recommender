214f6cb41f1795a4e1c8821fc2a97d1a6df549bf
unknown <dixondaniel434@gmail.com>
2025-01-11 13:36:26 +0200

    Clea the data from the csv files

--- backend/model/recommendation_system.py
+++ backend/model/recommendation_system.py
@@ -3,4 +3,11 @@ import pandas as pd
 movies = pd.read_csv('../data/movies.csv')
 ratings = pd.read_csv('../data/ratings.csv')
 
+# Removing duplicate rows
+movies.drop_duplicates(inplace=True)
+ratings.drop_duplicates(inplace=True)
+
+# Removing missing values
+movies.dropna(inplace=True)
+ratings.dropna(inplace=True)
 
