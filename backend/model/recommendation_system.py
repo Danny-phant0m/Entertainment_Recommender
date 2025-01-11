43d084dcef097c4a8c8050df0eb7d8972ab537ec
unknown <dixondaniel434@gmail.com>
2025-01-11 13:42:04 +0200

    Extract genres from the dataset and convert to numerical format

--- backend/model/recommendation_system.py
+++ backend/model/recommendation_system.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
 
 movies = pd.read_csv('../data/movies.csv')
 ratings = pd.read_csv('../data/ratings.csv')
@@ -11,3 +13,11 @@ ratings.drop_duplicates(inplace=True)
 movies.dropna(inplace=True)
 ratings.dropna(inplace=True)
 
+# Extracting the genres column
+genres = movies['genres']
+
+# Creating an instance of the OneHotEncoder
+encoder = OneHotEncoder()
+
+# Fitting and transforming the genres column
+genres_encoded = encoder.fit_transform(genres.values.reshape(-1, 1))
